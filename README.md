152b197855a3f632c3d27f01e31f4f902152bb12
Yicky <30880107+Yicky@users.noreply.github.com>
2017-08-11 10:22:20 -0400

    Made the code more concise and re-run the make

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Project Name: Guessing Game
-2017-08-10
-18:42
+2017-08-11
+10:20
 
 Number of lines of code in **guessinggame.sh**:
-47
+32
--- guessinggame.sh
+++ guessinggame.sh
@@ -2,23 +2,6 @@
 # File: guessinggame.sh
 
 num_files=$(ls -l | egrep ^- | wc -l)
-echo "Please type in your guess of the number of files in the current directory."
-read guess
-let err=$guess-$num_files
-
-if [[ $err -le -5 ]]
-then
-   echo "Your guess is too low."
-elif [[ $err -gt -5 && $err -lt 0 ]]
-then
-   echo "Your guess is close but lower than the actual number."
-elif [[ $err -gt 0 && $err -lt 5 ]]
-then
-   echo "Your guess is close but higher than the actual number."
-elif [[ $err -ge 5 ]]
-then
-   echo "Your guess is too high."
-fi
 
 function guessinggame {
    echo "Please type in your guess of the number of files in the current directory."
@@ -39,6 +22,8 @@ function guessinggame {
       echo "Your guess is too high."
    fi
 }
+
+guessinggame
 
 while [[ $err -ne 0 ]]
 do
